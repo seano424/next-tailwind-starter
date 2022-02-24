bf67c68d4fad61e307e488be4e4fb39013ea52e1
Sean OReilly <soreilly424@gmail.com>
2022-02-24 12:17:48 -0600

    adding dependencies to next starter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    debugScreens: {
+      position: ['bottom', 'right'],
+      style: {
+        backgroundColor: '#C0FFEE',
+        color: 'black',
+      },
+      prefix: 'screen: ',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-debug-screens', '@tailwindcss/forms', '@tailwindcss/aspect-ratio', '@tailwindcss/typography'),
+  ],
 }
