7779966df8c86488378cbb3945ef7f5962a43545
Sean OReilly <soreilly424@gmail.com>
2022-03-31 13:35:05 -0600

    Updated Layout, Font, Tailwind Config

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-
 export default function Document() {
   const devMode = process.env.NEXT_PUBLIC_DEV_MODE
   return (
-    <Html>
+    <Html className="scroll-smooth" lang="en">
       <Head />
       <body className={devMode ? 'debug-screens' : ''}>
         <Main />
