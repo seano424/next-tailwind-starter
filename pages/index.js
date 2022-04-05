ac59e0e20cbedf2fac8e6b4c51412c15a34559d4
Sean OReilly <soreilly424@gmail.com>
2022-04-05 12:33:38 -0500

    added tailwind prettier

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 export default function Home() {
   return (
     <div class="w-full py-20 container mx-auto">
-      <h1 className="text-7xl font-black text-center max-w-2xl mx-auto">Next Tailwind Starter Kit!!</h1>
-      <div className="grid grid-cols-3 gap-2 mt-20">
+      <h1 className="text-7xl font-black text-center max-w-2xl mx-auto">
+        Next Tailwind Starter Kit!!
+      </h1>
+      <div className="grid gap-2 mt-20 sm:grid-cols-2 lg:grid-cols-3">
         <div className="card">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestiae voluptatibus tempora reprehenderit velit accusamus, voluptates eligendi! 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestiae
+          voluptatibus tempora reprehenderit velit accusamus, voluptates
+          eligendi!
         </div>
 
         <div className="card">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eveniet voluptatibus nesciunt aliquid autem eos ipsum illum, provident repellat excepturi totam nisi est omnis fugit quis quisquam ea veniam quasi dolorum molestiae beatae exercitationem tempora in eius!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+          eveniet voluptatibus nesciunt aliquid autem eos ipsum illum, provident
+          repellat excepturi totam nisi est omnis fugit quis quisquam ea veniam
+          quasi dolorum molestiae beatae exercitationem tempora in eius!
         </div>
 
         <div className="card">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis eveniet voluptatibus nesciunt aliquid autem eos ipsum illum, provident repellat excepturi totam nisi est omnis fugit quis quisquam ea veniam quasi dolorum molestiae beatae exercitationem tempora in eius!
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+          eveniet voluptatibus nesciunt aliquid autem eos ipsum illum, provident
+          repellat excepturi totam nisi est omnis fugit quis quisquam ea veniam
+          quasi dolorum molestiae beatae exercitationem tempora in eius!
         </div>
       </div>
     </div>
