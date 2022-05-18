6c3bb47858044481197a9559facafb2c33a33ed2
Sean OReilly <soreilly424@gmail.com>
2022-05-18 09:49:52 -0500

    Updated React and Next Versions

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
+import { useEffect } from 'react'
+
 export default function Home() {
+  useEffect(() => {
+    console.log('Mounting...')
+
+    return () => console.log('Unmounting..')
+  }, [])
+  
   return (
     <div className="w-full container my-base">
       <h1 className="font-cabinet text-7xl font-black text-center max-w-2xl mx-auto">
