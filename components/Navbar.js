c8c145396e75971f84721062e1eefa3a4267c483
Sean OReilly <soreilly424@gmail.com>
2022-05-18 08:50:11 -0500

    Removing fixed Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center gap-6 w-full shadow-sm filter backdrop-blur-lg z-50 bg-white/90 fixed h-20 px-20">
+    <nav className="flex justify-between items-center gap-6 w-full shadow-sm filter backdrop-blur-lg z-50 bg-white/90 h-20 px-20">
       <Link href="/">
         <a className="link">🐳</a>
       </Link>
