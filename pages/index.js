2ae86258a4befda8d4549afc776efe2dd20a86d2
Sean OReilly <soreilly424@gmail.com>
2022-05-18 08:50:28 -0500

    Updating classes to use base and container

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-full py-20 container mx-auto">
-      <h1 className="text-7xl font-black text-center max-w-2xl mx-auto">
+    <div className="w-full container my-base">
+      <h1 className="font-cabinet text-7xl font-black text-center max-w-2xl mx-auto">
         Next Tailwind Starter Kit!!
       </h1>
     </div>
